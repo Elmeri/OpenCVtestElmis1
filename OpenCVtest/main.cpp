b732d45b8bb537379c9ae00175bf465d8eb09fb0
Elmeri Poikolainen <elmeripoikolainen@212-149-174-2.bb.dnainternet.fi>
2014-02-15 18:59:14 +0200

    Initial Commit

---
+++ OpenCVtest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCVtest
+//
+//  Created by Elmeri Poikolainen on 15.2.2014.
+//  Copyright (c) 2014 Elmeri Poikolainen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
